f512b1a10484b62f54623663187c4b2d2e9a4d36
Elijas <master.elijas@gmail.com>
2019-07-30 23:49:53 +0300

    Refactor modeling file io functions

--- src/modeling/logistic_regression.py
+++ src/modeling/logistic_regression.py
@@ -1,22 +1,24 @@
 import sys
 from pathlib import Path
 
-import joblib
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
+from src.evaluation.evaluate_classifier_performance import evaluate_classifier_performance
+from src.modeling._model_file_io import load_model, save_model
+
 ROOT_FOLDERPATH = Path.cwd().parent.parent
 sys.path.append(str(ROOT_FOLDERPATH))
 from src.preparation.load_data import load_dataset
 
-MODEL_FILEPATH = ROOT_FOLDERPATH / 'model' / 'logistic-regression.joblib'
+MODEL_FILEPATH = ROOT_FOLDERPATH / 'model' / 'naive-bayes.joblib'
 
 
-def train():
-    dataset = load_dataset()
-    pipeline = Pipeline([('vect', CountVectorizer(ngram_range=(1, 2))),
+def train(dataset=None):
+    dataset = dataset or load_dataset()
+    pipeline = Pipeline([('vect', CountVectorizer(ngram_range=(1, 3))),
                          ('tfidf', TfidfTransformer()),
                          ('clf', MultinomialNB())])
     param_grid = {
@@ -28,7 +30,7 @@ def train():
         'estimator': pipeline,
         'param_grid': param_grid,
         'cv': 2,
-        'scoring': 'f1_macro',
+        'scoring': 'accuracy',
         'verbose': 5,
         'n_jobs': -1,
     }
@@ -39,11 +41,14 @@ def train():
 
 def get_model():
     if MODEL_FILEPATH.exists():
-        with MODEL_FILEPATH.open('rb') as file:
-            classifier = joblib.load(file)
-            return classifier
-
+        return load_model(MODEL_FILEPATH)
     classifier = train()
-    with MODEL_FILEPATH.open('wb') as file:
-        joblib.dump(classifier, file)
+    save_model(MODEL_FILEPATH, classifier)
     return classifier
+
+
+if __name__ == '__main__':
+    dataset = load_dataset()
+    classifier = train(dataset)
+    save_model(MODEL_FILEPATH, classifier)
+    evaluate_classifier_performance(classifier, dataset)
