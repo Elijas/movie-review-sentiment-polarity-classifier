c29fc7b02373fefca715a84b6be5506938ba40d2
Elijas <master.elijas@gmail.com>
2019-08-02 19:40:34 +0300

    Refactor data folder structure

--- src/const.py
+++ src/const.py
@@ -14,13 +14,16 @@ DATASET_TEST_SPLIT_RATIO = 0.20
 
 class PATHS:
     ROOT_FOLDER = Path(os.path.dirname(os.path.realpath(__file__))).parent
-    CORPUS_FOLDER = ROOT_FOLDER / 'data'
-    CORPUS_POS = CORPUS_FOLDER / 'rt-polarity.pos'
-    CORPUS_NEG = CORPUS_FOLDER / 'rt-polarity.neg'
-    SPLIT_DATASET_FOLDER = CORPUS_FOLDER / 'split'
+    MODEL_FOLDER = ROOT_FOLDER / 'model'
+    DATA_FOLDER = ROOT_FOLDER / 'data'
+
+    RAW_DATASET_FOLDER = DATA_FOLDER / 'raw'
+    CORPUS_POS = RAW_DATASET_FOLDER / 'rt-polarity.pos'
+    CORPUS_NEG = RAW_DATASET_FOLDER / 'rt-polarity.neg'
+
+    SPLIT_DATASET_FOLDER = DATA_FOLDER / 'raw_structured'
     SPLIT_DATASET_TRN = SPLIT_DATASET_FOLDER / 'dataset-train.joblib'
     SPLIT_DATASET_TST = SPLIT_DATASET_FOLDER / 'dataset-test.joblib'
-    MODEL_FOLDER = ROOT_FOLDER / 'model'
 
 
 class JUPYTER:
